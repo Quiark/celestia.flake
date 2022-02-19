3aca8aa54d9c0be8bcd0a052a82f03c17b680cff
Roman Plášil <me@rplasil.name>
2022-02-19 17:18:33 +0000

    Upgrade to newer commit

--- flake.nix
+++ flake.nix
@@ -1,20 +1,14 @@
 {
     description = "Run Celestia";
     inputs.nixpkgs.url = github:NixOS/nixpkgs/21.11;
-    inputs.deploy-rs.url = "github:serokell/deploy-rs";
-    outputs = { self, nixpkgs, deploy-rs }: rec {
-
-	deploy.nodes.tufir = {
-      hostname = "tufir.valid";
-      sshUser = "root";
-      profiles.celes = {
-        user = "root";
-        path = deploy-rs.lib.x86_64-linux.activate.noop self.defaultPackage.x86_64-linux;
-      };
-    };
+    outputs = { self, nixpkgs }: rec {
 
     defaultPackage.x86_64-linux = appd;
 
+    # to update version, set sha256 attribute to empty string
+    # same with vendorSha256
+    # then run
+    # nix build .
     appd = 
         with import nixpkgs { system = "x86_64-linux"; };
         buildGoModule rec {
@@ -24,14 +18,14 @@
           src = fetchFromGitHub {
             owner = "celestiaorg";
             repo = "celestia-app";
-            rev = "c2adcb157cc39f58368a5779b01e655520f67890";
-            sha256 = "sha256-0zRWhXy/LMdtJz/+5AZZmfBO8XybiJ47svndwUq6+GA=";
+            rev = "78346556340b99bb4193703097f62779399e2ffe";
+            sha256 = "sha256-8Pra7ZQtbe5Z4/iEt6+o3A+quPVjZ/x1su1TPa3aYh4=";
           };
 
           buildInputs = [ go_1_17 packr ];
 
           runVend = false;
-          vendorSha256 = "sha256-R6dggu8f/fteiCSEe315m4iG4gnXj2SOLFz3hZbdVyo=";
+          vendorSha256 = "sha256-MFq2tHVRfS+Uc7e7y4+Xa3n9lTCFdKvHlzSl7E/qEDg=";
 
           meta = with lib; {
             description = "Modular data availability layer";
@@ -52,14 +46,14 @@
           src = fetchFromGitHub {
             owner = "celestiaorg";
             repo = "celestia-node";
-            rev = "f9ad11e4c651a78fc06ca697df0dcac10073e3c3";
-            sha256 = "sha256-cmdt5cU50O8vr4rvdFIZsMqR1fM/i5u/Hd0BL6JVCj8=";
+            rev = "7b59bfad569cb313de4d06c6af701d14b7060c40";
+            sha256 = "sha256-KPthhZIm8h2uhwyV6E4hitUUiE+T89crseMl+wf3ZzY";
           };
 
           buildInputs = [ go_1_17 packr ];
 
           runVend = false;
-          vendorSha256 = "sha256-5//vHtv2oPhoKvcYZ85EYFza1bscgrpFNQPzrDW/A10=";
+          vendorSha256 = "sha256-bxvcHZoF5nVHiAudLcDuJkXBh+EXfzb/cMC6Uguk/18";
 
           meta = with lib; {
             description = "Modular data availability layer, full node";
@@ -67,10 +61,8 @@
             license = licenses.asl20;
             maintainers = with maintainers; [ ];
             platforms = platforms.linux ++ platforms.darwin;
-            mainProgram = "celestia-node";
+            mainProgram = "celestia";
           };
       };
-
-
   };
 }
